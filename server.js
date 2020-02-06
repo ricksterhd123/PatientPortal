dfa290bcf2e32baa5ea119dc670c20c6416a3122
ricksterhd123 <ricksterhd123@gmail.com>
2020-02-06 10:39:26 +0000

    figured out heroku

--- server.js
+++ server.js
@@ -2,17 +2,12 @@
 index.js
 864163
 */
-const DEBUG = process.env.DEBUG || false;
-const express = require('express');
-const app = express();
-const port = DEBUG ? 5000 : 80;
 
-// Static files inside /public folder
-app.use(express.static('public'));
 
-// Routes
-// Index /GET
-app.get('/', (req, res) => res.render('public/index.html'));
+const express = require('express')
+const app = express()
+const port = 80
 
-// Setup server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
