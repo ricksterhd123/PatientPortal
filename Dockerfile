9fd34495211676cf1378931dc7b613f03fd49561
ricksterhd123 <ricksterhd123@gmail.com>
2020-04-10 22:41:06 +0100

    /login working, now need to define /callback etc

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM ubuntu:latest
 WORKDIR /usr/src/app
 RUN apt-get update
-RUN apt-get -y install libnss3-tools nodejs wget git sudo npm
+RUN apt-get -y install libnss3-tools wget git sudo curl
+RUN curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash - && sudo apt-get install -y nodejs
 RUN wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.1/mkcert-v1.4.1-linux-amd64
 RUN mv mkcert-v1.4.1-linux-amd64 mkcert
 RUN chmod 700 mkcert
--- server.js
+++ server.js
@@ -34,24 +34,42 @@ const fs = require('fs');
 const key = fs.readFileSync('./localhost-key.pem');
 const cert = fs.readFileSync('./localhost.pem');
 
+const { auth } = require("express-openid-connect");
+
+const config = {
+    required: false,
+    auth0Logout: true,
+    baseURL: "https://localhost:3000",
+    issuerBaseURL: "https://dev-088ewgr5.eu.auth0.com",
+    clientID: "3z4bJHQa2RdlzUn4P9JVrCTCOpRI06kU",
+    appSessionSecret: "a long, randomly-generated string stored in env"
+  };
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 app.set('views', __dirname + '/views');
 app.engine('html', engines.htmling);
 app.set('view engine', 'html');
-
 // Static files inside /public folder
 app.use(express.static('public'));
 
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+
+
+
+
 // Routes
 // Index /GET
-app.get('/', (req, res) => res.render('index.html'));
+//app.get('/', (req, res) => res.render('index.html'));
 app.get('/home', (req, res) => res.render('home.html'));
 app.get('/appointments', (req, res) => res.render('appointments.html'));
 app.get('/contact', (req, res) => res.render('contact.html'));
 app.get('/symptoms', (req, res) => res.render("symptoms.html"));
 app.get('/settings', (req, res) => res.render('settings.html'))
-
-app.get('/login', (req, res) => res.redirect("/home"));
-app.get('/logout', (req, res) => res.redirect('/'));
 
 // Setup server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
