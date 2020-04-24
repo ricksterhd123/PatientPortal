c3de82a4506a30f23f7564a7f54315b54378eb36
ricksterhd123 <ricksterhd123@gmail.com>
2020-04-24 18:26:25 +0100

    Removing unused packages

--- server.js
+++ server.js
@@ -5,43 +5,15 @@ index.js
 
 // Imports
 const express = require('express');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
 const engines = require('consolidate');
 const routes = require('./routes.js');
 const jwt = require('jsonwebtoken');
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 const secret = "shhhh"; // JWT secret (temporary until i figure out how to create HMAC SHA256 key)
 
-// Connection URL
-let url = 'mongodb://localhost:27017/myproject';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function (err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-
-  db.close();
-});
-
-let store = new MongoDBStore({
-  uri: 'mongodb://localhost:27017/connect_mongodb_session_test',
-  collection: 'mySessions'
-})
-
-store.on('error', (error) => console.log(error));
-app.use(session({
-  secret: 'this is a secret',
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
-  },
-  store: store,
-  resave: true,
-  saveUninitialized: true
-}));
 
 // Setup htmling template engine
 app.set('views', __dirname + '/views');
