2db06d50090b7056e618dde19e70faa807f401c9
ricksterhd123 <ricksterhd123@gmail.com>
2020-04-25 11:00:08 +0100

    Authentication working - just need to design api and register an admin

--- server.js
+++ server.js
@@ -3,18 +3,15 @@ index.js
 864163
 */
 
-// Imports
 const express = require('express');
 const engines = require('consolidate');
-const routes = require('./routes.js');
 const jwt = require('jsonwebtoken');
-
-
+const cookieParser = require('cookie-parser')
 const app = express();
 const port = process.env.PORT || 3000;
 const secret = "shhhh"; // JWT secret (temporary until i figure out how to create HMAC SHA256 key)
 
-
+app.use(cookieParser());
 // Setup htmling template engine
 app.set('views', __dirname + '/views');
 app.engine('html', engines.htmling);
@@ -29,16 +26,62 @@ app.post('/login', (req, res, next) => {
   let decoded = Buffer.from(auth, 'base64').toString();
   let [user, pass] = decoded.split(":");
 
-  if (user == "user" && pass == "password") {
-    let token = jwt.sign({
-      foo: 'bar'
-    }, secret);
-    res.setHeader('Set-Cookie', `jwt=${token}`);
-  }
+  let token = jwt.sign({
+    user: user
+  }, secret);
+
+  res.cookie('jwt', token, {httpOnly: true});
+  res.send(JSON.stringify({success:true}));
+
+  console.log("Generated token: " + token);
   console.log(`User: ${user}, Password: ${pass}`);
   next();
 });
 
+// Routes
+let routes = {};
+
+// req.isAuthenticated is provided from the auth router
+routes.index = function (req, res) {
+  let token = req.cookies.jwt
+  if (token) {
+    let valid = jwt.verify(token, secret);
+    if (valid) {
+      console.log(valid.user);
+      res.render('home.html');
+    }
+  } else {
+    res.render('login.html');
+  }
+}
+
+routes.appointments = function (req, res) { 
+    res.render('appointments.html');
+}
+
+routes.contact = function (req, res) {
+    res.render('contact.html');
+}
+
+routes.symptoms = function (req, res) {
+    res.render("symptoms.html");
+}
+
+routes.settings = function (req, res) {
+    res.render("settings.html");
+}
+
+// Not sure exactly what this does yet...
+routes.callback = function (req, res) {
+    res.redirect("/home");
+    console.log(req);
+}
+
+routes.logout = function(req, res) {
+  res.clearCookie('jwt');
+  res.redirect("/");
+}
+// End of routes
 // Web pages
 // TODO: make API once database is connected
 let r = {
@@ -47,7 +90,8 @@ let r = {
   "/contact": routes.contact,
   "/symptoms": routes.symptoms,
   "/settings": routes.settings,
-  "/callback": routes.callback
+  "/callback": routes.callback,
+  "/logout": routes.logout,
 };
 
 for (let [key, value] of Object.entries(r)) {
--- views/scripts/login.js
+++ views/scripts/login.js
@@ -3,14 +3,57 @@
 */
 
 class LoginPanel extends React.Component{
-    render(){
-        return <div id="login">
-                    <form action="/login">
-                        <input type="text" id="email" name="email address" placeholder="Your email address.."></input>
-                        <input type="password" id="password" name="password"></input>
+    constructor(props) {
+        super(props);
+        this.state = {success:true};
+        this.userInputHandle = this.userInputHandle.bind(this);
+        this.passInputHandle = this.passInputHandle.bind(this);
+        this.submitHandle = this.submitHandle.bind(this);
+    }
 
-                        <input id="login" type="submit" value="Login"></input>
-                    </form>
+    userInputHandle(event) {
+        this.setState({username: event.target.value})
+    }
+
+    passInputHandle(event) {
+        this.setState({password: event.target.value})
+    }
+
+    submitHandle(event) {
+        let xmlHttp = new XMLHttpRequest();
+        let self = this;
+        xmlHttp.open( "POST", "/login", false);
+        xmlHttp.setRequestHeader("Authorization", "Basic " + btoa(this.state.username+":"+this.state.password));
+        
+        xmlHttp.onload = function (e) {
+          if (xmlHttp.readyState === 4) {
+            if (xmlHttp.status === 200) {
+              console.log(xmlHttp.responseText);
+              let success = JSON.parse(xmlHttp.responseText).success;
+              if (success){
+                  location.reload();
+              } else {
+                  self.setState({success:false});
+              }
+            } else {
+              console.error(xmlHttp.statusText);
+            }
+          }
+        };
+        xmlHttp.onerror = function (e) {
+          console.error(xhr.statusText);
+        };
+        xmlHttp.send( null );
+        console.log("happened")  // send request, don't care about response
+        event.preventDefault()
+    }
+
+    render(){
+        return <div>
+                    <p>{this.state.success?"":"Invalid username or password!"}</p>
+                    <input type="text" name="username" placeholder="Username" onChange={this.userInputHandle}></input>
+                    <input type="password" name="password" onChange={this.passInputHandle}></input>
+                    <button type="submit" value="Login" onClick={this.submitHandle}>Login</button>
                 </div>
     }
 }
