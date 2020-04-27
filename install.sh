494debcbbb14c0a05890b7bb6224a3bd71f0ed32
ricksterhd123 <ricksterhd123@gmail.com>
2020-04-27 17:05:44 +0100

    Updated install script update every 5 minutes

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-(crontab -l ; echo "0 4 * * * git -C $DIR pull && docker-compose -f $DIR/docker-compose.yml up -d --build")| crontab -
+(crontab -l ; echo "*/5 * * * * git -C $DIR pull && docker-compose -f $DIR/docker-compose.yml up -d --build")| crontab -
 echo "Setup auto update in crontab"
 echo "Starting now..."
 docker-compose -f "docker-compose.yml" up -d --build
