e15e91de2173aca463e68a18c73b6d623ebfa0b6
ricksterhd123 <ricksterhd123@gmail.com>
2020-04-27 16:51:22 +0100

    Added install script for linux vps

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+(crontab -l ; echo "0 4 * * * git -C $DIR pull && docker-compose -f $DIR/docker-compose.yml up -d --build")| crontab -
+echo "Setup auto update in crontab"
+echo "Starting now..."
+docker-compose -f "docker-compose.yml" up -d --build
+echo "Done"
+
