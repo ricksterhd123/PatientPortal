ad1b214ef68805d5a1b2a4dde64766425c54e0f7
ricksterhd123 <ricksterhd123@gmail.com>
2020-04-30 14:49:21 +0100

    Forgot to add <div id="menu"></div>

---
+++ views/components/register.js
@@ -0,0 +1,71 @@
+/* jshint esversion: 6 */
+
+class RegisterPanel extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {success:true};
+        this.userInputHandle = this.userInputHandle.bind(this);
+        this.passInputHandle = this.passInputHandle.bind(this);
+        this.submitHandle = this.submitHandle.bind(this);
+    }
+
+    userInputHandle(event) {
+        this.setState({username: event.target.value})
+    }
+
+    passInputHandle(event) {
+        this.setState({password: event.target.value})
+    }
+
+    submitHandle(event) {
+        let xmlHttp = new XMLHttpRequest();
+        let self = this;
+        xmlHttp.open( "POST", "/api/register", false);
+        xmlHttp.setRequestHeader("Authorization", "Basic " + btoa(this.state.username+":"+this.state.password));
+        
+        xmlHttp.onload = function (e) {
+          if (xmlHttp.readyState === 4) {
+            if (xmlHttp.status === 200) {
+              console.log(xmlHttp.responseText);
+              let success = JSON.parse(xmlHttp.responseText).success;
+              if (success){
+                  window.location.href = "/";
+              } else {
+                  self.setState({success:false});
+              }
+            } else {
+              console.error(xmlHttp.statusText);
+            }
+          }
+        };
+        xmlHttp.onerror = function (e) {
+          console.error(xhr.statusText);
+        };
+        xmlHttp.send( null );
+        console.log("happened")  // send request, don't care about response
+        event.preventDefault()
+    }
+
+    render(){
+        return <div>
+                    <h1>The Nuffield Center GP</h1>
+                    <p>{this.state.success?"":"Failed to login please try again"}</p>
+                    <p>Username:</p>
+                    <input type="text" name="username" placeholder="Username" onChange={this.userInputHandle}></input>
+                    <p>Password:</p>
+                    <input type="password" name="password" onChange={this.passInputHandle}></input>
+                    <p>Repeat password:</p>
+                    <input type="password" name="repeat password"></input>
+                    <button type="submit" value="Login" onClick={this.submitHandle}>Login</button>
+                    <p>Not registered?</p>
+                    <a href="/register">Sign up</a>
+                </div>
+    }
+}
+
+function showRegisterPanel(){
+    const element = <RegisterPanel/>;
+    ReactDOM.render(element, document.getElementById("menu"));
+}
+
+showRegisterPanel();
