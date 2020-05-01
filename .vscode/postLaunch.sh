1af9549286a12c0483ddb801cff3e80e03008603
ricksterhd123 <ricksterhd123@gmail.com>
2020-05-01 17:39:24 +0100

    Updated vscode config

---
+++ .vscode/postLaunch.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+docker-compose -f .vscode/docker-compose.yml stop
+cd patientportal
+echo "Cleaning up ..."
+for file in 'public/scripts'/*; do
+    echo "Removing $file"
+    rm $file
+done
+rm -r public/scripts
+echo "Finished"
--- .vscode/preLaunch.sh
+++ .vscode/preLaunch.sh
@@ -1,5 +1,9 @@
+#! /bin/bash
 cd patientportal
-npm install
+if [ ! -f package-lock.json ]; then
+    echo "package-lock.json not found, running npm install"
+    npm install
+fi
 npm run compile
 cd ..
 docker-compose -f .vscode/docker-compose.yml up -d --build
