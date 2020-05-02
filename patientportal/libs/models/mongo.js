76c697ce0ac005f830201ee1f76d52bda95bd7f5
ricksterhd123 <ricksterhd123@gmail.com>
2020-05-02 22:42:11 +0100

    environment variable switch from test -> DEBUG

--- patientportal/libs/models/mongo.js
+++ patientportal/libs/models/mongo.js
@@ -1,6 +1,6 @@
 const client = require('mongodb').MongoClient;
-console.log(process.env.test);
-const hostName = process.env.test == "true" ? "localhost" : "mongo";
+console.log(process.env.DEBUG);
+const hostName = process.env.DEBUG == "true" ? "localhost" : "mongo";
 const URL = `mongodb://${process.env.user}:${process.env.pass}@${hostName}:27017`;
 console.log(`URL: ${URL}`);
 const options = {useUnifiedTopology: true};
