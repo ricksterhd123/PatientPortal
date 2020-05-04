274e4864ddc0a034a8df6467609e58e5ff48ddf1
ricksterhd123 <ricksterhd123@gmail.com>
2020-05-04 19:04:06 +0100

    Added multiple accounts for testing

--- patientportal/test/test.js
+++ patientportal/test/test.js
@@ -5,16 +5,27 @@ const roles = require('../libs/roles');
 const assert = require('assert');
 const request = require('supertest');
 const {describe, it} = require("mocha");
-const username = 'test';
-const password = 'password';
+let accounts = {
+    patient: {
+        username: 'patient',
+        password: 'password'
+    },
+    clinician: {
+        username: 'clinician',
+        password: 'password'
+    },
+    Admin: {
+        username: 'admin',
+        password: 'password'
+    }
+};
 
 describe('Checking if authentication works', function() {
-    
     it('User can register', async function() {
         try {
             const response = await request(app)
                 .post('/api/register')
-                .send({ username: username, password: password })
+                .send({ username: accounts.admin.username, password: accounts.admin.password })
                 .set('Accept', 'application/json')
                 .expect('Content-Type', 'application/json; charset=utf-8')
                 .expect(200);
@@ -29,7 +40,7 @@ describe('Checking if authentication works', function() {
         try {
             const response = await request(app)
                 .post('/api/login')
-                .set("Authorization", "Basic " + Buffer.from(username + ":" + password).toString('base64'))
+                .set("Authorization", "Basic " + Buffer.from(accounts.admin.username + ":" + accounts.admin.password).toString('base64'))
                 .set('Accept', 'application/json')
                 .expect('Content-Type', 'application/json; charset=utf-8')
                 .expect(200);
@@ -45,12 +56,29 @@ describe('Checking if authentication works', function() {
         try {
             const response = await request(app)
                 .post('/api/login')
-                .set("Authorization", "Basic " + Buffer.from(username + ":" + password).toString('base64'))
+                .set("Authorization", "Basic " + Buffer.from(accounts.admin.username + ":" + accounts.admin.password).toString('base64'))
                 .set('Accept', 'application/json')
                 .expect('Content-Type', 'application/json; charset=utf-8')
                 .expect(200);
-            assert.strictEqual(response.body.result.username, username);
+            assert.strictEqual(response.body.result.username, accounts.admin.username);
             assert.strictEqual(response.body.result.role, roles.ADMIN);
+            assert.strictEqual(response.body.error, null);
+        }
+        catch (err) {
+            assert.strictEqual(err, null);
+        }
+    });
+
+    it('Second user registered is a patient', async function() {
+        try {
+            const response = await request(app)
+                .post('/api/register')
+                .send({ username: accounts.patient.username, password: accounts.patient.password })
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(200);
+            assert.strictEqual(response.body.result.username, accounts.patient.username);
+            assert.strictEqual(response.body.result.role, roles.USER);  // todo rename to roles.PATIENT
             assert.strictEqual(response.body.error, null);
         }
         catch (err) {
@@ -62,5 +90,7 @@ describe('Checking if authentication works', function() {
 // TODO
 // describe('Checking if messages work', function() {
 //     const agent = request.agent(app);
-//     it('Can send message', async function)
+//     it('User can send message', function () {
+
+//     });
 // });
