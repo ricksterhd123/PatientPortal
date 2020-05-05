fcfceb006e51ccffb26c69225dbc784f4dbf6605
ricksterhd123 <ricksterhd123@gmail.com>
2020-05-05 07:56:12 +0100

    Updated .vscode for quicker debugging and made the homepage mobile first

--- patientportal/views/components/admin.js
+++ patientportal/views/components/admin.js
@@ -1,22 +1,21 @@
 
 class AdminPanel extends React.Component {
     render() {
-        return <div id={this.props.id}>
-            <h1>Admin settings</h1>
-            <div id="links">
-                <a id="admin-users" className="big-button" href="/admin/users">Manage users</a>
-                <a id="admin-schedules" className="big-button" href="/admin/schedules">Manage schedules</a>
-                <a id="admin-messages" className="big-button" href="/admin/messages">Manage messages</a>
-                <a id="admin-appointments" className="big-button" href="/admin/appointments">Manage appointments</a>
-                <a id="admin-settings" className="big-button" href="/admin/settings">Misc</a>
+        return <div id={this.props.id} class="container btn-group-vertical">
+                <h2>{this.props.title}</h2>
+                <button type="button" class="btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/admin/users"}}>Manage users</button>
+                <button type="button" class="btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/contact"}}>Messages</button>
+                <button type="button" class="btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/admin/appointments"}}>Manage appointments</button>
+                <button type="button" class="btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/admin/articles"}}>Manage articles</button>
+                <button type="button" class="btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/settings"}}>Settings</button>
+                <button type="button" class="btn btn-danger btn-lg btn-block" onClick={()=>{window.location.href = "/api/logout"}}>Logout</button>
             </div>
-        </div>
     }
 }
 
 function showAdminPanel() {
-    let adminPanel = <AdminPanel id="admin-panel"/>;
-    ReactDOM.render(adminPanel, document.getElementById("admin-menu"));
+    let adminPanel = <AdminPanel id="admin-panel" title="Admin settings"/>;
+    ReactDOM.render(adminPanel, document.getElementById('menu'));
 }
 
 showAdminPanel();
--- patientportal/views/components/home.js
+++ patientportal/views/components/home.js
@@ -1,12 +1,12 @@
 class IndexNavigation extends React.Component {
     
     render(){
-        return <div class="center container btn-group-vertical">
-                <button type="button" class="center btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/appointments"}}>Appointments</button>
-                <button type="button" class="center btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/contact"}}>Contact GP</button>
-                <button type="button" class="center btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/symptoms"}}>Symptom search</button>
-                <button type="button" class="center btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/settings"}}>Settings</button>
-                <button type="button" class="center btn btn-danger btn-lg btn-block" onClick={()=>{window.location.href = "/api/logout"}}>Logout</button>
+        return <div class="container btn-group-vertical">
+                <button type="button" class="btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/appointments"}}>Appointments</button>
+                <button type="button" class="btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/contact"}}>Contact GP</button>
+                <button type="button" class="btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/symptoms"}}>Symptom search</button>
+                <button type="button" class="btn btn-primary btn-lg btn-block" onClick={()=>{window.location.href = "/settings"}}>Settings</button>
+                <button type="button" class="btn btn-danger btn-lg btn-block" onClick={()=>{window.location.href = "/api/logout"}}>Logout</button>
             </div>
     }
 }
